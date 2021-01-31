999b0b20aafd2da740ee0fb4a240bcf95bacf436
lhxone <lhxone@outlook.com>
2021-01-31 12:29:53 +0800

    Script

--- TestDemo.m
+++ TestDemo.m
@@ -1,16 +1,5 @@
-A=[1 2 3;4 5 4;9 8 7];
-B=[3 3 3;2 4 9;1 3 1];
-a=2;
-
-x1=A+a;
-x2=A/a;
-x3=A./a;
-x4=A^a;
-x5=A.^a;
-C=A';
-
-y1=A+B;
-y2=A*B;
-y3=A.*B;
-y4=A/B
-y5=A./B;
+for index = 1:10
+    x=linspace(0, 10 ,101);
+    plot(x,sin(x+i));
+    print(gcf, '-deps', strcat('plot',num2str(i),'.png'));
+end
---
+++ untitled.m
@@ -0,0 +1,7 @@
+a=[1 2 3];
+a
+% for index = 1:10
+%     x=linspace(0, 10 ,101);
+%     plot(x,sin(x+i));
+%     print(gcf, '-deps', strcat('plot',num2str(i),'.ps'));
+% end
