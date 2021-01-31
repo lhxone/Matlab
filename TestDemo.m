b99ff1a63cae534ddd06aee8ce0861ba35b546cc
lhxone <lhxone@outlook.com>
2021-01-31 22:44:53 +0800

    Plot

--- TestDemo.m
+++ TestDemo.m
@@ -1,5 +1,11 @@
-for index = 1:10
-    x=linspace(0, 10 ,101);
-    plot(x,sin(x+i));
-    print(gcf, '-deps', strcat('plot',num2str(i),'.png'));
-end
+minbnd = -4*pi;  
+maxbnd = 4*pi;  
+t = minbnd:0.1*pi:maxbnd;  
+plot(t, sin(t), 'g', 'Linewidth', 2);  
+line([minbnd, maxbnd], [0, 0]);    %绘制x轴  
+axis([-10, 10, -2, 2])             %定义显示的坐标区间:x在(-10,10)之间，y在(-2,2)之间  
+grid on;  
+title('sin(x)');  
+xlabel('x');  
+ylabel('sin(x)');  
+print('-dpng','sin.png');          %保存为png图片，在Matlab当前的工作目录下  
